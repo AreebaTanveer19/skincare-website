857274eecff9102438a70eca6e17b20222c8ff32
Areeba Tanveer <areeba.mughal263@gmail.com>
2025-07-10 17:14:18 +0500

    myorder route added

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', require('./routes/orders'));
+app.use('/api/myorders', require('./routes/orders'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
