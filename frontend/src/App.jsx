df0e45445ff5afd71aa325352b6a201e00d43788
Areeba Tanveer <areeba.mughal263@gmail.com>
2025-09-13 18:42:40 +0500

    vercel.json added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Product";
